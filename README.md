a406dc4b8379f606dedece5e4ddf5defb575ec2f
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2023-02-04 09:44:20 +0000

    Use cargo cross

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gap the mind Rmk
+
+## Run
+
+    cross run --target armv7-unknown-linux-gnueabihf --bin gtm
+
+## Development
+
+    brew install arm-linux-gnueabihf-binutils
--- xtask/src/bin/main.rs
+++ xtask/src/bin/main.rs
@@ -2,8 +2,35 @@ use clap::{Command, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Cli {}
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// setup ssh key
+    Sshetup {
+        /// remarkable ip address, either newtork over usb or wifi
+        #[arg(short, long, default_value_t = String::from("10.11.99.1"))]
+        ip: String,
+
+        /// ssh user
+        #[arg(short, long, default_value_t = String::from("root"))]
+        user: String,
+
+        #[arg(short, long)]
+        password: Option<String>,
+    },
+}
 
 fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Commands::Sshetup { ip, user, password }) => todo!(),
+        one => todo!(),
+    }
+
     Ok(())
 }
